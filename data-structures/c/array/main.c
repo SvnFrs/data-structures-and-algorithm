14e5c3c2354506a015585e138f923300b5589961
“SvnFrs” <thaidvqce170410@fpt.edu.vn>
2024-03-20 01:45:06 +0700

    First commit

---
+++ data-structures/c/array/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int array[100];
+
+    
+}
+
+void printMenu() {
+    printf("Array practice in C\n");
+    printf("1. Add item\n");
+    printf("2. Remove item\n");
+    printf("3. Update item\n");
+    printf("4. Exit\n");
+
+    printf("\tChoose one wisely: ");
+}
